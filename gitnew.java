077c97b2b7a8883b440b0276efa00869dbe9569e
krishan <krishanchoudhary06042001@gmail.com>
2025-04-22 15:10:41 +0530

    change in gitnew file

--- gitnew.java
+++ gitnew.java
@@ -3,5 +3,7 @@ public static void main(String[] args) {
     System.out.println("hello");
     System.out.println("welcome");
 }
-    
+    public void m1(){
+        System.out.println("m1");
+    }
 }
